aeb7fef708c4d273cc22e27c9775c91220ac93e3
Eugene <89818121788@mail.ru>
2022-09-29 21:15:31 +0300

    HW done

---
+++ src/main/java/ru/hogwarts/school/controllers/FacultyController.java
@@ -0,0 +1,65 @@
+package ru.hogwarts.school.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.hogwarts.school.models.Faculty;
+import ru.hogwarts.school.services.FacultyService;
+
+import java.util.Collection;
+
+@RestController
+@RequestMapping("faculty")
+public class FacultyController {
+    private final FacultyService facultyService;
+
+    public FacultyController(FacultyService facultyService) {
+        this.facultyService = facultyService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Faculty> createFaculty(@RequestBody Faculty faculty) {
+        return ResponseEntity.ok(facultyService.createFaculty(faculty));
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Faculty> getFacultyInfo(@PathVariable Long id) {
+        Faculty findFaculty = facultyService.findFaculty(id);
+        if (findFaculty == null) {
+            ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+        return ResponseEntity.ok(findFaculty);
+    }
+
+    @GetMapping
+    public ResponseEntity<Collection<Faculty>> getAllFaculties() {
+        return ResponseEntity.ok(facultyService.getAllFaculties());
+    }
+
+    @PutMapping
+    public ResponseEntity<Faculty> editFaculty(@RequestBody Faculty faculty) {
+        Faculty editeFaculty = facultyService.editFaculty(faculty);
+        if (editeFaculty == null) {
+            ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+        return ResponseEntity.ok(editeFaculty);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Faculty> deleteFaculty(@PathVariable Long id) {
+        Faculty deleteFaculty = facultyService.deleteFaculty(id);
+        if (deleteFaculty == null) {
+            ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+        return ResponseEntity.ok(deleteFaculty);
+    }
+
+    @GetMapping("/color")
+    public ResponseEntity<Collection<Faculty>> findFacultiesByColor(@RequestParam String color) {
+        Collection<Faculty> findFacultiesByColor = facultyService.findFacultyByColor(color);
+        if (findFacultiesByColor == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+        return ResponseEntity.ok(findFacultiesByColor);
+    }
+}
