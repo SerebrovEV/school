53adcf3d69495e3b57d5126734f020233e7f95b4
Eugene <89818121788@mail.ru>
2022-10-20 18:51:15 +0300

    HW4.2 correction

--- scripts423.sql
+++ scripts423.sql
@@ -1,6 +1,6 @@
 SELECT s.name, s.age, f.name
 FROM student s
-FULL JOIN faculty f ON s.faculty_id = f.id;
+LEFT JOIN faculty f ON s.faculty_id = f.id;
 
 SELECT s.id, s.name, s.age, a.id, a.media_type
 FROM student s
